c283a79af91337b90346915c855da9fa76fedd30
Puspita Vabna <sharif7761@gmail.com>
2022-02-19 02:34:08 +0600

    installed telescope

---
+++ resources/views/topics.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Topics</title>
+</head>
+<body>
+	<ul>
+		<li>Config file</li>
+		<li>Service provider file</li>
+		<li>public vendor asset file</li>
+		<li>migration file</li>
+	</ul>
+</body>
+</html>
